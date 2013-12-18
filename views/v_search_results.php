ed4be94c9c119c2a9932b44fe4a20fc4b2e77e34
Dadi <nafi.dadi@gmail.com>
2013-12-17 22:43:27 -0600

    modified how search results are displayed

--- views/v_search_results.php
+++ views/v_search_results.php
@@ -1,20 +1,25 @@
 <?php foreach($hotelinfo as $hotel): ?>
-        <ul>
-                <li><a href='http://<?=$hotel['website']?>'><?=$hotel['hotel_name']?></a></li>
-                <li><?=$hotel['street_address']?></li>
-                <li><?=$hotel['city']?>, <?=$hotel['state']?> <?=$hotel['zipcode']?></li>
-                <li><?=$hotel['hotel_id']?></li>
-		<li><a href='http://<?=$hotel['website']?>'><?=$hotel['website']?></a></li>
-                <li><?=$hotel['email']?></li>
-        </ul>
+	<address>
+		<a href='http://<?=$hotel['website']?>'><?=$hotel['hotel_name']?></a><br />
+                <?=$hotel['street_address']?><br />
+		<?=$hotel['city']?>, <?=$hotel['state']?> <?=$hotel['zipcode']?><br />
+                contact: <?=$hotel['hotel_id']?>  <?=$hotel['email']?><br />
+                website: <a href='http://<?=$hotel['website']?>'><?=$hotel['website']?></a>
+	</address>
 
 	<div id='comment'>
-		<p><?php echo $hotel['nick_name']?> posted:</p>
-		<time datetime="<?php echo Time::display($hotel['created'],'Y-m-d G:i')?>">
-        		<?php echo Time::display($hotel['created'])?>
-    		</time>
+		<?php if($hotel['content'] != NULL): ?>
+		<article>
+			<time datetime="<?php echo Time::display($hotel['created'],'Y-m-d G:i')?>">
+        			<?php echo Time::display($hotel['created'])?>
+    			</time>
+			<?php echo "posted by ".$hotel['nick_name'] .": "?>
+			<br />
 
-    		<p><?php echo $hotel['content']?></p>
+    			<?php echo $hotel['content']?>
+		</article>
+		<?php endif; ?>
+
 		<?php if($user): ?>
 		<section id='new_post'>
 			<form name='newpost' id='newpost' method='POST' action='/posts/p_add'>
